a708d1284556b31182c57a3603b3c0a61cd85021
Julio Matias <juliocesark2@hotmail.com>
2023-07-27 10:24:12 -0300

    concluindo requisito 7

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const butaoAdicionar = document.querySelector('#criar-tarefa');
 const entradaTexto = document.querySelector('#texto-tarefa');
 const listaOrdenada = document.querySelector('#lista-tarefas');
-
 const redefineEntrada = () => {
   entradaTexto.value = '';
 };
+
+// 5 - Adicione um botão e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+// 6 - Adicione três novas tarefas e ordene todas as tarefas da lista por ordem de criação
+
 const adicionaTextoALista = () => {
   butaoAdicionar.addEventListener('click', () => {
     const textoEntrada = entradaTexto.value;
@@ -15,3 +18,20 @@ const adicionaTextoALista = () => {
   });
 };
 adicionaTextoALista();
+
+// 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza
+
+const mudaCorDeFundo = (elemento) => {
+  const itemLista = elemento;
+  if (itemLista.style.backgroundColor !== 'rgb(128, 128, 128)') {
+    itemLista.style.backgroundColor = 'rgb(128, 128, 128)';
+  } else {
+    itemLista.style.backgroundColor = '';
+  }
+};
+listaOrdenada.addEventListener('click', (event) => {
+  console.log(event.target.tagName);
+  if (event.target.tagName === 'LI') {
+    mudaCorDeFundo(event.target);
+  }
+});
