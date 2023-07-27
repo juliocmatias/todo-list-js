0ff2dbe30e56226589239b51540f6a7c1c03697b
Julio Matias <juliocesark2@hotmail.com>
2023-07-27 00:36:09 -0300

    add códigos do requisito 5 ao script.js, com função de para adicionar evento de click no botão e outra função de redefinir a entrada

---
+++ script.js
@@ -0,0 +1,16 @@
+const butaoAdicionar = document.querySelector('#criar-tarefa');
+const entradaTexto = document.querySelector('#texto-tarefa');
+const listaOrdenada = document.querySelector('#lista-tarefas');
+const redefineEntrada = () => {
+    entradaTexto.value = '';
+};
+const adicionaTextoALista = () => {
+  butaoAdicionar.addEventListener('click', () => {
+    const textoEntrada = entradaTexto.value;
+    const lista = document.createElement('li');
+    lista.innerHTML = textoEntrada;
+    listaOrdenada.appendChild(lista);
+    redefineEntrada();
+  });
+};
+adicionaTextoALista();
